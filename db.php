bd7b517c29f6681d17a335440b460f870167a7c9
Tarun Gupta <soul.tarun@gmail.com>
2017-03-31 16:53:44 +0530

    db updated

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","register");
+$host = "vp2016.cmxqtqdqyohf.us-east-1.rds.amazonaws.com";
+$port = "3306";
+$username = "karan";
+$password = "karan_ums";
+$database = "karan_ums";
+
+$con = mysqli_connect($host, $username, $password, $database);
 // Check connection
 if (mysqli_connect_errno())
   {
